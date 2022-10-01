eabf949c598d3aff7894717f92c8479db3a531b8
hamzahamza11 <sabirha00@gmail.com>
2022-10-01 20:28:34 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// sendFile will go here
+app.get('/.well-known/acme-challenge/Xk0hmR3jSyOeuW6MvWv5NDNMqkqE8y3aEcaxHCRiMrM', function(req, res) {
+  res.sendFile(path.join(`${__dirname}/public`, '/index.html'));
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);

a2b294974e8f81896d1320fd79ff6e57ad1084a2
hamzahamza11 <sabirha00@gmail.com>
2022-10-01 20:41:33 +0100

    first commit 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 // sendFile will go here
 app.get('/.well-known/acme-challenge/Xk0hmR3jSyOeuW6MvWv5NDNMqkqE8y3aEcaxHCRiMrM', function(req, res) {

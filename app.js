420f336faa56e36416280e3e786c01282ad24fa5
hamzahamza11 <sabirha00@gmail.com>
2022-10-01 20:51:33 +0100

    first commit 2355

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 80;
 
 // sendFile will go here
-app.get('/.well-known/acme-challenge/WRsJ_6AvzXDq1Ow29h1r2TUQ-UsVclZbeCrsjGEj0BQ', function(req, res) {
+app.get('/.well-known/acme-challenge/67j8tx2ijStLmgXo3ZmJ4N7Y9Cdv-zZt2477xMg4Nwo', function(req, res) {
   res.sendFile(path.join(`${__dirname}/public`, '/index.html'));
 });
 
